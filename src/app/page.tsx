94e6481cc11ffe5fd9e796cb63cf418a4175b370
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-18 21:47:06 -0400

    Added: test components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import { Hero_Button, Navbar } from "./Components/Globals";
 
 export default function Home() {
   return (
     <main className="">
+      <Navbar/>
+      <Hero_Button text_feild="something"/>
       <a href='/about'>About</a>
       home
     </main>

9a2215d133164a9fa337959ac04175d12cdd814a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-19 21:31:48 -0400

    Removed: sample placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className="">
       <Navbar/>
-      <Hero_Button text_feild="something"/>
-      <a href='/about'>About</a>
-      home
     </main>
   )
 }

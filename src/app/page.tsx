2c7015f6d2ce3c7e06761ac1286e1d8f5b026bf0
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-20 23:57:55 -0400

    Added: Start of Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,20 @@ import { Hero_Button, Navbar } from "./Components/Globals";
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar/>
+    <main className="overflow-x-hidden">
+      <Navbar />
+      <section className="mt-2 mx-0">
+        <div className="flex mr-5 text-5xl">
+          <p>
+            <span className="text-mainContrast font-extrabold">Project </span>
+            management at your fingertips
+          </p>
+        </div>
+        <div className="flex flex-row-reverse mr-5 mt-5">
+          <Hero_Button text_feild="Sign Up Now!" />
+        </div>
+        <div></div>
+      </section>
     </main>
-  )
+  );
 }

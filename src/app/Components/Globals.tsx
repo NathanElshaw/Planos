a1ac7618a52cefe89296c2be4a05435b2735a626
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-20 23:57:35 -0400

    Added: stlye type to hero button

--- src/app/Components/Globals.tsx
+++ src/app/Components/Globals.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import Hamburger from "hamburger-react";
+import { Squash as Hamburger } from "hamburger-react";
 import { NextPage } from "next";
 import React from "react";
 //InterFaces
 interface Hero_Button_Props {
   text_feild: string;
+  styles?: string;
 }
 //Global Components
 export const Navbar = () => {
@@ -31,15 +32,16 @@ export const Navbar = () => {
                 <Hero_Button text_feild="Sign Up!" />
               </li>
             </ol>
-            <div className="flex justify-center items-center rounded-md max-h-[50px] max-w[50px] sm:hidden bg-mainContrast">
+            <div className="flex justify-center items-center max-h-[40px] rounded-md sm:hidden bg-mainContrast">
               <Hamburger
                 toggled={dropdown_Toggled}
                 toggle={() => {
                   set_dropdown_Toggle(!dropdown_Toggled);
                 }}
                 color="#272727"
-                size={26}
-                duration={0.5}
+                size={20}
+                distance="lg"
+                duration={0.3}
               />
             </div>
             <div
@@ -69,9 +71,11 @@ export const Navbar = () => {
 };
 
 export const Hero_Button: NextPage<Hero_Button_Props> = (props) => {
-  const { text_feild } = props;
+  const { text_feild, styles } = props;
   return (
-    <button className="px-4 py-2 rounded-md bg-mainContrast text-black ease-in-out duration-200 hover:bg-fadeContrast">
+    <button
+      className={`px-4 py-2 rounded-md bg-mainContrast text-black ease-in-out duration-200 hover:bg-fadeContrast ${styles}`}
+    >
       {text_feild}
     </button>
   );

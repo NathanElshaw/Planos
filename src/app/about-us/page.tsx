97b29473e65cd7b93bd637211236a3c567429f1c
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-22 20:00:31 -0400

    Added: about us page

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../Components/Globals";
+
+export default function About() {
+  return (
+    <main>
+      <Navbar />
+      <section id="sign-up-page"></section>
+    </main>
+  );
+}

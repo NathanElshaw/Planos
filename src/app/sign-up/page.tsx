4a3970cc8e465646c58f0fa8f7d96a86e691a3d0
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-22 20:01:44 -0400

    Added: sign up page

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../Components/Globals";
+
+export default function SignUp() {
+  return (
+    <main>
+      <Navbar />
+      <section id="sign-up-page"></section>
+    </main>
+  );
+}

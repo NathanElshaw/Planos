37bf358eec3e69a23d6f2dc3e924cb1c6dbf2d7a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-22 20:01:00 -0400

    Added: info page

---
+++ src/app/info/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../Components/Globals";
+
+export default function Info() {
+  return (
+    <main>
+      <Navbar />
+      <section id="info"></section>
+    </main>
+  );
+}

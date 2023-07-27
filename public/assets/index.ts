56601dbcedc753fed5ff7630e72cee95722e969d
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-26 21:23:48 -0400

    Added: svg's and global export

---
+++ public/assets/index.ts
@@ -0,0 +1,21 @@
+import accounting from "./accounting.svg";
+import business_network from "./business-network.svg";
+import collaboration from "./collaboration.svg";
+import communication from "./communication.svg";
+import estimates from "./estimates.svg";
+import exchange from "./exchange.svg";
+import inspection from "./inspection.svg";
+import schedule from "./schedule.svg";
+import total_sales from "./total-sales.svg";
+
+export {
+  accounting,
+  business_network,
+  collaboration,
+  communication,
+  estimates,
+  exchange,
+  inspection,
+  schedule,
+  total_sales,
+};

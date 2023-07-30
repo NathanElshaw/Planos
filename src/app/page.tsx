c32902186385658347d97c0d168de2fa7b23a091
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-30 16:15:20 -0400

    Added: iphone image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { Footer, Hero_Button, Navbar } from "./Components/Globals";
 import Image from "next/image";
 import collaboration from "../../public/assets/collaboration.svg";
 import schedule from "../../public/assets/schedule.svg";
 import estimates from "../../public/assets/estimates.svg";
+import { Iphone } from "../../public/assets";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Home() {
+  const [image_Loaded, set_Image_Loaded] = useState<boolean>(false);
+
   return (
     <main className="overflow-x-hidden">
       <Navbar />
       <section className="mt-2 mx-2" id="main_hero">
         <div className="grid grid-cols-4 mr-5 text-5xl">
           <div className="col-span-3 ml-2">
-            <span className="text-mainContrast font-extrabold">Project </span>
-            management at your fingertips
+            <span className="text-mainContrast font-extrabold">
+              Project management <br />
+            </span>
+            at your fingertips
           </div>
         </div>
         <div className="flex flex-row-reverse mr-5 mt-5">
@@ -20,6 +29,28 @@ export default function Home() {
             <Hero_Button text_feild="Sign Up Now!" />
           </a>
         </div>
+        <motion.div
+          className="z-[-1] min-h-full"
+          animate={{
+            opacity: image_Loaded === true ? 1 : 0,
+            transition: { ease: "linear" },
+          }}
+        >
+          <Image
+            src={Iphone}
+            alt="iphone"
+            className="absolute z-[-1] right-[0%] top-[40%]"
+            onLoad={(event) => {
+              const target: any = event.target;
+              if (target.src.indexOf("data:image/gif;base64") < 0) {
+                setTimeout(() => {
+                  set_Image_Loaded(!image_Loaded);
+                }, 1000);
+              }
+            }}
+          />
+        </motion.div>
+        <div className="h-[350px]" />
       </section>
       <section>
         <div id="info">

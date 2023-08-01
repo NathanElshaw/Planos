6dc6d1c0811fabb27d05d327704af6053d588f3f
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-31 21:34:58 -0400

    Added: contact page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,11 @@
+import { Footer, Navbar } from "../Components/Globals";
+
+export default function ContactUs() {
+  return (
+    <main>
+      <Navbar />
+      <section id="contact-us-main"></section>
+      <Footer />
+    </main>
+  );
+}

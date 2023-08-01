c89e2b0642a1ae466313dbce3b8745aaee43cfb8
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-31 21:35:20 -0400

    Added: use cases page

---
+++ src/app/use-cases/page.tsx
@@ -0,0 +1,11 @@
+import { Footer, Navbar } from "../Components/Globals";
+
+export default function UseCases() {
+  return (
+    <main>
+      <Navbar />
+      <section id="use-cases-main"></section>
+      <Footer />
+    </main>
+  );
+}

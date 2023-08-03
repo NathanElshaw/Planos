5339510c0ad9fb99480abbefea0fde56b0f33f2e
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-02 20:06:15 -0400

    Added: info text

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,4 +1,4 @@
-import { Footer, Navbar } from "../Components/Globals";
+import { Footer, Hero_Button, Navbar } from "../Components/Globals";
 import Image from "next/image";
 import collaboration from "../../../public/assets/collaboration.svg";
 import {
@@ -13,86 +13,124 @@ export default function Info() {
   return (
     <main>
       <Navbar />
-      <section id="info" className="mx-4">
-        <div>
+      <section id="info" className="mx-6 overflow-x-hidden">
+        <div className="mt-2">
           <h2 className="text-3xl">Info</h2>
         </div>
-        <div>
-          <div className="flex justify-center my-2 mx-2">
-            <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
-              <Image priority src={schedule} alt="Collaboration" />
+        <div className="flex flex-col gap-y-2">
+          <div>
+            <div className="flex justify-center my-2 mx-2">
+              <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
+                <Image priority src={schedule} alt="Collaboration" />
+              </div>
+            </div>
+            <div className="text-center mx-2">
+              <span className="font-semibold text-lg">Scheduling: </span>
+              <p>
+                Our app empowers efficient project management. Real-time
+                updates, prioritized tasks, and BIM integration ensure seamless
+                execution. Stay ahead with instant notifications. Elevate your
+                projects with smart scheduling
+              </p>
             </div>
           </div>
-          <div className="text-center mx-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            doloribus atque fuga! Iusto asperiores nesciunt perspiciatis
-            corrupti ab architecto cum libero aspernatur deserunt! Quasi eum,
-            quis dignissimos beatae est ipsum!
+          <div>
+            <div className="flex justify-center my-2 mx-2">
+              <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
+                <Image priority src={collaboration} alt="Collaboration" />
+              </div>
+            </div>
+            <div className="text-center mx-2">
+              <span className="font-semibold text-lg">Collaboration: </span>
+              <p>
+                Embrace our app for seamless collaboration and peak
+                productivity. Real-time messaging, project management, and
+                document sharing keep teams connected. BIM integration ensures
+                data synchronization. Stay ahead with instant updates.
+                Experience the future of project management!
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center my-2 mx-2">
+              <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
+                <Image priority src={estimates} alt="Collaboration" />
+              </div>
+            </div>
+            <div className="text-center mx-2">
+              <span className="font-semibold text-lg">
+                Invoice and Job Payroll management:{" "}
+              </span>
+              <p>
+                Our app simplifies invoice and payroll management. Real-time
+                tracking, automation, and precise control ensure efficiency.
+                Take charge of your projects with ease!
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center my-2 mx-2">
+              <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
+                <Image priority src={business_network} alt="Collaboration" />
+              </div>
+            </div>
+            <div className="text-center mx-2">
+              <span className="font-semibold text-lg">Customer Relations</span>
+              <p>
+                Elevate your construction experience with our app's seamless
+                communication platform. Stay connected with customers in
+                real-time, ensuring clarity and transparency throughout the
+                project. Share progress updates, address concerns, and receive
+                valuable feedback effortlessly. Build trust and foster positive
+                relationships, leading to satisfied clients and successful
+                projects. Experience the power of customer communication -
+                revolutionize your construction journey today!
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center my-2 mx-2">
+              <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
+                <Image priority src={inspection} alt="Collaboration" />
+              </div>
+            </div>
+            <div className="text-center mx-2">
+              <span className="font-semibold text-lg">
+                Continuous Checklists
+              </span>
+              <p>
+                Embrace the power of checklists and meticulous checks with our
+                app. Sail through your project without a hitch as you ensure
+                nothing is overlooked. From tasks to quality control, our system
+                keeps you on track. Minimize bumps, delays, and errors with ease
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-center my-2 mx-2">
+              <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
+                <Image priority src={exchange} alt="Collaboration" />
+              </div>
+            </div>
+            <div className="text-center mx-2">
+              <span className="font-semibold text-lg">Seemless Payments</span>
+              <p>
+                - Experience the magic of seamless payments with our app. Say
+                goodbye to payment headaches and project delays. Streamline
+                financial transactions, from vendor payments to project
+                expenses, with real-time tracking and automation. Boost cash
+                flow, improve relationships, and ensure smooth operations.
+                Embrace the future of construction projects
+              </p>
+            </div>
           </div>
         </div>
-        <div>
-          <div className="flex justify-center my-2 mx-2">
-            <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
-              <Image priority src={collaboration} alt="Collaboration" />
-            </div>
-          </div>
-          <div className="text-center mx-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            doloribus atque fuga! Iusto asperiores nesciunt perspiciatis
-            corrupti ab architecto cum libero aspernatur deserunt! Quasi eum,
-            quis dignissimos beatae est ipsum!
-          </div>
-        </div>
-        <div>
-          <div className="flex justify-center my-2 mx-2">
-            <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
-              <Image priority src={estimates} alt="Collaboration" />
-            </div>
-          </div>
-          <div className="text-center mx-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            doloribus atque fuga! Iusto asperiores nesciunt perspiciatis
-            corrupti ab architecto cum libero aspernatur deserunt! Quasi eum,
-            quis dignissimos beatae est ipsum!
-          </div>
-        </div>
-        <div>
-          <div className="flex justify-center my-2 mx-2">
-            <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
-              <Image priority src={business_network} alt="Collaboration" />
-            </div>
-          </div>
-          <div className="text-center mx-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            doloribus atque fuga! Iusto asperiores nesciunt perspiciatis
-            corrupti ab architecto cum libero aspernatur deserunt! Quasi eum,
-            quis dignissimos beatae est ipsum!
-          </div>
-        </div>
-        <div>
-          <div className="flex justify-center my-2 mx-2">
-            <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
-              <Image priority src={inspection} alt="Collaboration" />
-            </div>
-          </div>
-          <div className="text-center mx-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            doloribus atque fuga! Iusto asperiores nesciunt perspiciatis
-            corrupti ab architecto cum libero aspernatur deserunt! Quasi eum,
-            quis dignissimos beatae est ipsum!
-          </div>
-        </div>
-        <div>
-          <div className="flex justify-center my-2 mx-2">
-            <div className="bg-darkGrey px-3 py-3 rounded-full drop-shadow-md shadow-black">
-              <Image priority src={exchange} alt="Collaboration" />
-            </div>
-          </div>
-          <div className="text-center mx-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            doloribus atque fuga! Iusto asperiores nesciunt perspiciatis
-            corrupti ab architecto cum libero aspernatur deserunt! Quasi eum,
-            quis dignissimos beatae est ipsum!
+        <div className="mt-4 flex flex-col">
+          <p className="text-l">Interested in signing up for early access?</p>
+          <div className="flex flex-row-reverse mt-4">
+            <a href="/sign-up">
+              <Hero_Button text_feild="Sign Up for Beta!" />
+            </a>
           </div>
         </div>
       </section>

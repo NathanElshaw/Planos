3b25389731ee9f846f470d7db8ee97d22b55a3e9
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-18 23:49:38 -0400

    Added: light grey

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
       mainContrast: `#C4FE76`,
       fadeContrast: `#9EEA6C`,
       darkGrey: "#333333",
+      lightGrey: "#7F7F7F",
       errorRed: `#CC0000`,
     },
   },

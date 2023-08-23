8b8b84de827633d5a533eb37e62715b183a43487
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-08-22 21:11:57 -0400

    Added: responsiveness

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,13 +5,13 @@ export default function About() {
     <main>
       <Navbar />
       <section id="about-us-title">
-        <div className="mt-2 ml-2 flex">
+        <div className="mt-2 ml-2 sm:ml-4 md:ml-8 flex">
           <h1 className="text-3xl font-semibold text-mainContrast">About</h1>
         </div>
       </section>
       <section
         id="about-us-main-content"
-        className="flex justify-center flex-col mx-4 my-10"
+        className="flex justify-center flex-col mx-4 my-10 sm:mx-20 lg:mx-60"
       >
         <div className="text-center  mt-62">
           <p>

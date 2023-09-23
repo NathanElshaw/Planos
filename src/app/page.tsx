3518b1656c6f06338669b3fca8a6495e33293bf2
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-09-22 23:33:57 -0400

    Added: time line info and link to full time line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,10 +65,7 @@ export default function Home() {
           </a>
         </div>
       </section>
-      <section
-        id="quick-info"
-        className="px-2 pb-4 text-base lg:mx-60 border border-t-0 border-l-0 border-r-0 rounded-b-md border-b-lightGrey"
-      >
+      <section id="quick-info" className="px-2 pb-4 text-base lg:mx-60">
         <div
           id="info-title"
           className="flex justify-center pb-2 mt-4 border border-l-0 border-r-0 border-t-0 rounded-b-md border-b-mainWhite"
@@ -144,10 +141,7 @@ export default function Home() {
           </a>
         </div>
       </section>
-      <section
-        id="how-it-works"
-        className="px-2 pb-4 text-base lg:mx-60 border border-t-0 border-l-0 border-r-0 rounded-b-md border-b-lightGrey"
-      >
+      <section id="how-it-works" className="px-2 pb-4 text-base lg:mx-60">
         <div className="flex justify-center pb-2 mt-4 border border-l-0 border-r-0 border-t-0 rounded-b-md border-b-mainWhite">
           <h2 className="text-3xl">How it works</h2>
         </div>
@@ -183,38 +177,47 @@ export default function Home() {
       </section>
       <section id="timeline" className="mx-2 mt-10 text-base lg:mx-60">
         <div className="flex justify-center text-center py-2 mt-4 border border-l-0 border-r-0 border-t-0 rounded-b-md border-b-mainWhite">
-          <h2 className="text-3xl">Timeline projection</h2>
+          <h2 className="text-3xl">Timeline Projection</h2>
         </div>
-        <VerticalTimeline layout="2-columns">
+        <VerticalTimeline>
           <VerticalTimelineElement
             contentStyle={{ background: "#272727", color: "#fff" }}
             contentArrowStyle={{ borderRight: "7px solid rbg(33,150,250)" }}
-            date="End Q4 2023"
+            date="End of December 2023"
             textClassName="text-mainWhite"
           >
-            <h2 className="text-mainWhite">Closed beta MVP</h2>
+            <h2 className="text-mainWhite">Closed Beta MVP</h2>
             <p>
               Towards the end of Q4 we project to have a collaborative calendar
               where companies can communicate where they may need worker to
               finish jobs before the deadline. Furthermore will help to make
               sure previous step were completed before sending the next phase
-              crew to find out that it was not ready.
+              crew to find out that it was not ready. This will beta will only
+              be avaliable to those who sign up prior to the deadline (Mid
+              December)
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             contentStyle={{ background: "#272727", color: "#fff" }}
             contentArrowStyle={{ borderLeft: "7px solid rbg(33,150,250)" }}
-            date="End Q4 2023"
+            date="End of March 2024"
           >
-            <h2 className="text-mainWhite">Title</h2>
+            <h2 className="text-mainWhite">Open Beta</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              hic amet possimus excepturi, voluptatem doloribus incidunt ab
-              officiis recusandae assumenda perspiciatis perferendis natus
-              quaerat a id similique nulla eaque et!
+              By the end of Q1 of 2024 we plan to have a fully ready mvp for
+              open beta testing. We hope to include the fully functional
+              calendar as well as communication feature to encourage more
+              collaboration. Additionally we plan to have fully digital time
+              cards for better reading for both keeping track of job hours and
+              to streamline payroll.
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
+        <div className="flex flex-row-reverse my-2">
+          <a href="/timeline">
+            <Hero_Button text_feild="See Full Timeline" />
+          </a>
+        </div>
       </section>
       <Footer />
     </main>

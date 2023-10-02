a8f44cc1415d3920d2066f90aa04f2159e037aa6
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-10-01 22:31:43 -0400

    Added: material and template

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -10,9 +10,51 @@ export default function About() {
         </div>
       </section>
       <section
-        id="about-us-main-content"
+        id="about-us-company"
         className="flex justify-center flex-col mx-4 my-10 sm:mx-20 lg:mx-60"
       >
+        <div className="my-4 ml-2">
+          <h2 className="text-3xl font-semibold text-mainContrast">
+            Meet the idea
+          </h2>
+        </div>
+        <div className="text-center  mt-62">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            accusamus quaerat pariatur quisquam suscipit veniam officiis vel,
+            fugiat facilis tenetur sunt eos reiciendis? Ducimus, vel vero?
+            Minima optio suscipit facere? Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Commodi minus nulla explicabo natus
+            iusto repellat, ex adipisci numquam expedita fugit in exercitationem
+            quis consectetur culpa et unde. Vel, neque consequuntur?
+          </p>
+        </div>
+        <div className="text-center mt-6">
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            accusamus quaerat pariatur quisquam suscipit veniam officiis vel,
+            fugiat facilis tenetur sunt eos reiciendis? Ducimus, vel vero?
+            Minima optio suscipit facere?
+          </p>
+        </div>
+        <div className="text-center  mt-6">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            accusamus quaerat pariatur quisquam suscipit veniam officiis vel,
+            fugiat facilis tenetur sunt eos reiciendis? Ducimus, vel vero?
+            Minima optio suscipit facere?
+          </p>
+        </div>
+      </section>
+      <section
+        id="about-us-founder"
+        className="flex justify-center flex-col mx-4 my-10 sm:mx-20 lg:mx-60"
+      >
+        <div className="my-4 ml-2">
+          <h2 className="text-3xl font-semibold text-mainContrast">
+            Meet the founder
+          </h2>
+        </div>
         <div className="text-center  mt-62">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
